c0926e2bbdb145c18da54eeff3b6ba223a19b01f
Lysandro Carioca <lysandroc@gmail.com>
2019-06-06 22:20:23 -0300

    understading the basic of rabbitmq

---
+++ sample-rabbitmq-consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace sample_rabbitmq_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var con = factory.CreateConnection())
+            {
+                using (var channel = con.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Blog",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine(message);
+                    };
+
+                    channel.BasicConsume(queue: "Blog", 
+                        autoAck: true, 
+                        consumer: consumer);
+
+                    Console.WriteLine("Mensagem recebida!");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ sample-rabbitmq/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace sample_rabbitmq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var con = factory.CreateConnection())
+            {
+                using (var channel = con.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Blog",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = "understading how rabbit works";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "Blog",
+                        basicProperties:null,
+                        body:body);
+
+                    Console.WriteLine("Mensagem enviada!");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
